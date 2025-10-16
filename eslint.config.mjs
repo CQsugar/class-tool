ff4fddf875102b3a8b9e80c9aa68a84ab01beb1a
CQSuagr <1186765234@qq.com>
2025-10-16 10:15:48 +0800

    init

---
+++ eslint.config.mjs
@@ -0,0 +1,29 @@
+import { dirname } from 'path'
+import { fileURLToPath } from 'url'
+import { FlatCompat } from '@eslint/eslintrc'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+})
+
+const eslintConfig = [
+  ...compat.config({
+    extends: ['next/core-web-vitals', 'next/typescript', 'prettier'],
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'off',
+    },
+    ignorePatterns: [
+      'node_modules/**',
+      '.next/**',
+      'out/**',
+      'build/**',
+      'next-env.d.ts',
+      '**/__tests__/**',
+    ],
+  }),
+]
+
+export default eslintConfig
